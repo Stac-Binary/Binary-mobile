64f24ec90045f58f4ca60458dd6e5a16fdf3710c
Jominseo <minseocho0309@gmail.com>
2020-09-24 16:59:27 +0900

    [add] style

---
+++ binary_flutter/lib/constants/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+const kTokenKey = "TOKEN_KEY";
+const kRTokenKey = "REF_TOKEN_KEY";
+
+const kNanum = TextStyle(fontFamily: "Nanum");
+const kNanumBold = TextStyle(fontFamily: "Nanum", fontWeight: FontWeight.w700);
+const kNanumLight = TextStyle(fontFamily: "Nanum", fontWeight: FontWeight.w500);
+const kNanumThin = TextStyle(fontFamily: "Nanum", fontWeight: FontWeight.w300);
+
+const kPink = Color(0xFFFF8181);
+const kLightGrey = Color(0xFFCCCCCC);
+const kRed = Color(0xFFDE5B5B);
+const kWhite = Color(0xFFF9F9F9);
+const kGrey = Color(0xFFC2C2C2);
+const kLightBlack = Color(0xFF707070);
